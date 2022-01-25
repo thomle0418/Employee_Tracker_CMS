be684a8a6d2d3072455f24f08f838f28767c8f1a
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-25 15:41:58 -0700

    created db and department, roles, and employee tables

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS  employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name_ VARCHAR (30),
+);
+
+CREATE TABLE role(
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(30), 
+    salary DECIMAL, 
+    department_id INT,
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+);
