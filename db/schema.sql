1801f6ae073d7cd456796444cd0d79f9f00c6705
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-25 18:09:33 -0700

    removed sequlize and express because they are irrelavent to this project. Renamed server.js to app.js, added foreign key to db.

--- db/schema.sql
+++ db/schema.sql
@@ -9,17 +9,23 @@ CREATE TABLE department (
     name_ VARCHAR (30),
 );
 
-CREATE TABLE role(
+CREATE TABLE roles (
     id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(30), 
     salary DECIMAL, 
     department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN key (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
