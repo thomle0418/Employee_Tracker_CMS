57fa73be5d4b02c94884bc6da930c778f14ccb60
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-26 16:56:09 -0700

    added add-role functionality. Always user to input department, salary, and role.

--- app.js
+++ app.js
@@ -122,3 +122,89 @@ const addDepartment = ()=>{
             }
     });
 };
+
+const validateNum = function(num){
+    if (/^[0-9]+$/.test(num)){
+        return true;
+    }
+    return 'Invalid input. Only numeric values are accepted'
+};
+
+const addRole = ()=>{
+    let departments =[];
+    let departmentNames = ['No existing departms in database'];
+    
+    connection.query(
+        'SELECT * FROM department', (err,res)=>{
+            if(err) throw err;
+            if(res.length>0){
+                if(departmentNames[0]=== 'No existing departments in database'){
+                    departmentNames.splice(0,1);
+                }
+                res.forEach(({id, name})=>{
+                    departments.push({id, name});
+                    departmentNames.push(`${id} | ${name}`);
+                });
+            }
+            inquire.prompt([
+                {
+                name: 'roleTitle',
+                type: 'input', 
+                message: 'New role title:',
+            }
+        ])
+        .then((answer)=>{
+            let roleExists = false;
+            let roleTitle= answer.roleTitle.trim();
+            connection.query('SELECT title FROM role', (err, res)=>{
+                if(err) throw err;
+                res.forEach((item)=>{ //Prevents user from creating roles with the same title
+                    if(item.title===roleTitle){
+                        roleExists= true;
+                    };
+                });
+                if(roleExists===true){
+                    console.log(`The role titled '${roleTitle}' already exists.`);
+                    setTimeout(addMenu, 1000);
+                }else{
+                    inquirer.prompt([{
+                        name: 'roleSalary', 
+                        type: 'input', 
+                        message: 'New role salary:',
+                        validate: validateNum
+                    },{
+                        name: 'roleDepartment', 
+                        type: 'list', 
+                        message: 'New role department:',
+                        choices: departmentNames
+                    }
+                ])
+                .then((answers)=>{
+                    let departmentId= '';
+                    let splitAnswer= answers.roleDepartment.split(' ');
+                    let departmentName = splitAnswer.splice(2).join(' ').trim(); 
+                    if(answers.roleDepartment === 'No existing departments in database'){
+                        departmentId = null;
+                        departmentName= 'No existing departments in database';
+                    }else{
+                        for (let i = 0; i< departments.length; i++){
+                            if(departments[i].name === departmentName){
+                                departmentId = departments[i].id;
+                            };
+                        };
+                    };
+                    connection.query(
+                        `INSERT INTO role(title, salary, department_id) VALUES (?,?,?)`, [roleTitle, parseInt(answers.roleSalary), departmentId],
+                        (err,res) => {
+                            if(err) throw err;
+                            console.log(`'${roleTitle}' role (${answers.roleSalary}/yr | ${departmentName}) successfully added to database.`);
+                            setTimeout(mainMenu, 1000);
+                        }
+                    );
+                });
+                }
+            });
+        });
+        }
+    );
+};
